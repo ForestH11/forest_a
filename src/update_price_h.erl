58a15cbd0e553f25c028d0c9de585a4ebe394ca6
Benjamin Jenson <benjamin.jenson11@yahoo.com>
2024-03-20 20:50:15 -0600

    added logic to handlers to send responses to users

--- src/update_price_h.erl
+++ src/update_price_h.erl
@@ -9,10 +9,11 @@ init(Req0, Opts) ->
     %expecting an array with some number of grocery 
     %%items in quotes
     Price_update = jsx:decode(Data),
-    io:format("~p~n",[Price_update]),
-    Response = erpc:call('b@64.23.134.161',update_price_server,set_prices_of,[Price_update]),
-    Req = cowboy_req:reply(200, #{
-        <<"content-type">> => <<"text/json">>
-    }, Response, Req0),
+    % io:format("~p~n",[Price_update]),
+    Response = case erpc:call('b@64.23.134.161',update_price_server,set_prices_of,[Price_update]) of
+        ok -> 200;
+        _ -> 500
+    end,
+    Req = cowboy_req:reply(Response, Req0),
     {ok, Req, Opts}.
 % <<"[\"a\",\"list\",\"of\",\"words\"]">>
