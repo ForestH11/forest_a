7b29df94f011088227d86fc94e148bdec3a851db
Benjamin Jenson <benjamin.jenson11@yahoo.com>
2024-03-21 08:39:34 -0600

    accept data from the internet

--- src/update_price_h.erl
+++ src/update_price_h.erl
@@ -4,12 +4,12 @@
 -export([init/2]).
 
 init(Req0, Opts) ->
-    Data = <<"{\"apple\":3.0,\"bannana\":2.15}">>,
-    % {ok,Data,_} = cowboy_req:read_body(Req0),
+    % Data = <<"{\"apple\":3.0,\"bannana\":2.15}">>,
+    {ok,Data,_} = cowboy_req:read_body(Req0),
     %expecting an array with some number of grocery 
     %%items in quotes
     Price_update = jsx:decode(Data),
-    % io:format("~p~n",[Price_update]),
+    io:format("~p~n",[Price_update]),
     Response = case erpc:call('b@64.23.134.161',update_price_server,set_prices_of,[Price_update]) of
         ok -> 200;
         _ -> 500
