84633c4df0513bc867d0670e9bf5b15e8fc0423a
Benjamin Jenson <benjamin.jenson11@yahoo.com>
2024-04-03 15:22:51 -0600

    changed pathing of erpc call

--- src/customer_request_h.erl
+++ src/customer_request_h.erl
@@ -11,11 +11,12 @@ init(Req0, Opts) ->
     {ok,Data,_} = cowboy_req:read_body(Req0),
     %expecting an array with some number of grocery 
     %%items in quotes
-    Price_List = jsx:decode(Data),
-    % io:format("~p~n",[Price_List]),
-    Price_Data = erpc:call('b@64.23.134.161',customer_request_server,get_prices_of,[Price_List]),
-    % io:format("~p~n",[Price_Data]),
-    Response = jsx:encode(Price_Data),
+    Price_list = jsx:decode(Data),
+    % io:format("~p~n",[Price_list]),
+    % Price_data = erpc:call('b@64.23.134.161',customer_request_server,get_prices_of,[Price_list]),
+    Price_data = erpc:call('b@64.23.134.161', dispatch, customer,[Price_list]),
+    % io:format("~p~n",[Price_data]),
+    Response = jsx:encode(Price_data),
     Req = cowboy_req:reply(200, #{
         <<"content-type">> => <<"text/json">>
     }, Response, Req0),
